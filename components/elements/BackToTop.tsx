74032f592faaf050254ed5d83f802b980211a3db
hulk-higakijin <hm385.chejptks@gmail.com>
2022-12-17 11:05:14 +0900

    refactor: components/elementsのts化

---
+++ components/elements/BackToTop.tsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+
+function BackToTop() {
+  const [hasScrolled, setHasScrolled] = useState<boolean>(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", onScroll);
+    return () => {
+      window.removeEventListener("scroll", onScroll);
+    };
+  });
+
+  const onScroll = () => {
+    if (window.scrollY > 100 && !hasScrolled) {
+      setHasScrolled(true);
+    } else if (window.scrollY < 100 && hasScrolled) {
+      setHasScrolled(false);
+    }
+  };
+
+  return (
+    <>
+      {hasScrolled && (
+        <a
+          id="scrollUp"
+          href="#top"
+          style={{ position: "fixed", zIndex: 2147483647 }}
+        >
+          <i className="fi-rr-arrow-small-up" />
+        </a>
+      )}
+    </>
+  );
+}
+export default BackToTop;
